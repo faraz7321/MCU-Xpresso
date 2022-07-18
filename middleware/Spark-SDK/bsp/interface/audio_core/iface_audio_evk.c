8f293ac59346bbef3a5fd95365f9b0a74b38fca1
Faraz_Ahmad <faraz.ahmad@streamunlimited.com>
2022-07-18 12:50:43 +0200

    Sin wave produced

--- middleware/Spark-SDK/bsp/interface/audio_core/iface_audio_evk.c
+++ middleware/Spark-SDK/bsp/interface/audio_core/iface_audio_evk.c
@@ -14,6 +14,13 @@
 #include "sac_api.h"
 #include "swc_api.h"
 
+// Sin wave config
+const double AMP = 32767.0;
+const double SAMPLE_RATE = 48000.0;
+const double FREQUENCY = 500.0;
+static volatile uint16_t sample_count = 0;
+static int16_t wave[48000];
+
 /* PRIVATE FUNCTION PROTOTYPES ************************************************/
 static uint16_t ep_max98091_action_produce(void *instance, uint8_t *samples, uint16_t size);
 static void ep_max98091_start_produce(void *instance);
@@ -30,13 +37,15 @@ static void ep_swc_stop(void *instance);
 void iface_audio_swc_endpoint_init(sac_endpoint_interface_t *swc_producer_iface,
                                    sac_endpoint_interface_t *swc_consumer_iface)
 {
-    if (swc_producer_iface != NULL) {
+    if (swc_producer_iface != NULL)
+    {
         swc_producer_iface->action = ep_swc_produce;
         swc_producer_iface->start = ep_swc_start;
         swc_producer_iface->stop = ep_swc_stop;
     }
 
-    if (swc_consumer_iface != NULL) {
+    if (swc_consumer_iface != NULL)
+    {
         swc_consumer_iface->action = ep_swc_consume;
         swc_consumer_iface->start = ep_swc_start;
         swc_consumer_iface->stop = ep_swc_stop;
@@ -46,13 +55,15 @@ void iface_audio_swc_endpoint_init(sac_endpoint_interface_t *swc_producer_iface,
 void iface_audio_max98091_endpoint_init(sac_endpoint_interface_t *max98091_producer_iface,
                                         sac_endpoint_interface_t *max98091_consumer_iface)
 {
-    if (max98091_producer_iface != NULL) {
+    if (max98091_producer_iface != NULL)
+    {
         max98091_producer_iface->action = ep_max98091_action_produce;
         max98091_producer_iface->start = ep_max98091_start_produce;
         max98091_producer_iface->stop = ep_max98091_stop_produce;
     }
 
-    if (max98091_consumer_iface != NULL) {
+    if (max98091_consumer_iface != NULL)
+    {
         max98091_consumer_iface->action = ep_max98091_action_consume;
         max98091_consumer_iface->start = ep_max98091_start_consume;
         max98091_consumer_iface->stop = ep_max98091_stop_consume;
@@ -73,11 +84,25 @@ void iface_audio_critical_section_init(queue_critical_cfg_t *queue_critical)
  *  @param[in]  size      Size of samples to produce in bytes.
  *  @return Number of bytes produced (always 0 since production is delayed).
  */
+uint8_t dummy[256];
 static uint16_t ep_max98091_action_produce(void *instance, uint8_t *samples, uint16_t size)
 {
     (void)instance;
 
-    evk_audio_sai_read_non_blocking(samples, size);
+    int16_t *samples = (int16_t *)data;
+    uint16_t samples_count = size / 2;
+    for (int n = 0; n < samples_count / 2; n = n + 2)
+    {
+        samples[n] = wave[sample_count];
+        samples[n + 1] = samples[n];
+        sample_count += 1;
+        if (sample_count >= 48000)
+        {
+            sample_count = 0;
+        }
+    }
+
+    evk_audio_sai_read_non_blocking(dummy, size);
 
     return 0;
 }
@@ -90,6 +115,11 @@ static void ep_max98091_start_produce(void *instance)
 {
     (void)instance;
 
+    // generates and assign sin values
+    for (int n = 0; n < 48000; n++)
+    {
+        wave[n] = (double)(AMP * sinf((M_PI * 2 / SAMPLE_RATE) * FREQUENCY * n));
+    }
     evk_audio_sai_start_read_non_blocking();
 }
 
@@ -152,7 +182,7 @@ static void ep_max98091_stop_consume(void *instance)
 static uint16_t ep_swc_produce(void *instance, uint8_t *samples, uint16_t size)
 {
     uint8_t *payload = NULL;
-    uint8_t  payload_size;
+    uint8_t payload_size;
     swc_error_t err;
     ep_swc_instance_t *inst = (ep_swc_instance_t *)instance;
     (void)size;
@@ -179,11 +209,14 @@ static uint16_t ep_swc_consume(void *instance, uint8_t *samples, uint16_t size)
     ep_swc_instance_t *inst = (ep_swc_instance_t *)instance;
 
     swc_connection_get_payload_buffer(inst->connection, &buf, &err);
-    if (buf != NULL) {
+    if (buf != NULL)
+    {
         memcpy(buf, samples, size);
         swc_connection_send(inst->connection, buf, size, &err);
         return size;
-    } else {
+    }
+    else
+    {
         return 0;
     }
 }
